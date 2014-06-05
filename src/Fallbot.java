f43671d17e39ab990f54fe8aff20aeaf5307c583
Fallout301 <fallout.3.for.life@gmail.com>
2014-06-05 10:23:03 -0400

    Conversion from PircBotX to PircBot, much cleaner.

--- src/Fallbot.java
+++ src/Fallbot.java
@@ -1,65 +1,40 @@
+import org.jibble.pircbot.PircBot;
+import org.mariadb.jdbc.*;
+
 /**
  * Created by Fallout301 on 3/14/14.
  */
+public class Fallbot extends PircBot {
+    String Name = "Fallbot301";
 
-import org.pircbotx.Channel;
-import org.pircbotx.Configuration;
-import org.pircbotx.PircBotX;
-import org.pircbotx.UtilSSLSocketFactory;
-import org.pircbotx.cap.TLSCapHandler;
-import org.pircbotx.dcc.ReceiveChat;
-import org.pircbotx.exception.IrcException;
-import org.pircbotx.hooks.ListenerAdapter;
-import org.pircbotx.hooks.WaitForQueue;
-import org.pircbotx.hooks.events.IncomingChatRequestEvent;
-import org.pircbotx.hooks.events.MessageEvent;
-import org.pircbotx.hooks.events.PrivateMessageEvent;
-import org.pircbotx.hooks.managers.ListenerManager;
-import org.pircbotx.hooks.types.GenericMessageEvent;
-import org.pircbotx.hooks.Event;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.sql.SQLException;
-
-import com.google.common.*;
-import org.pircbotx.Utils;
-import org.pircbotx.output.OutputChannel;
-import org.pircbotx.output.OutputIRC;
-
-import org.mariadb.jdbc.*;
-
-public class Fallbot extends ListenerAdapter {
-    @Override
-    public void onGenericMessage(final GenericMessageEvent event) throws Exception {
-        //Hello world
-        //This way to handle commands is useful for listeners that listen for multiple commands
-        if (event.getMessage().startsWith("?hello")){
-            event.respond("Hello World!");
-        }
+    public Fallbot(){
+        this.setName(Name);
     }
-
-
-    public static void main(String args[]){
-
-    Configuration config = new Configuration.Builder()
-         .setName("Fallbot301")
-         .setLogin("fallbot301")
-         .addListener(new Fallbot())
-         //.setServer(<redacted login information, will cause compile problems until fixed>)
-         .addAutoJoinChannel("#fallbot301")
-         .buildConfiguration();
-        PircBotX bot = new PircBotX(config);
-        try{
-        bot.startBot();
+    public void onMessage(String channel, String sender,
+                          String login, String hostname, String message) {
+        //Eh, why not.
+        if (message.equalsIgnoreCase("time")) {
+            String time = new java.util.Date().toString();
+            sendMessage(channel, sender + ": The time is now " + time);
         }
-        catch(Exception e){
-            e.printStackTrace();
+        if (message.equalsIgnoreCase("!dismiss")& channel.contains(sender)){
+            sendMessage(channel,"Understood, peace out!");
+            try{
+            wait(1,1);
+            }catch(Exception e){
+                partChannel(channel);
+            }
         }
+        if(message.equalsIgnoreCase("!join")& channel.contains(getNick().toLowerCase())){
+            sendMessage(channel,"I shall arrive shortly!");
+            try{
+                wait(1,1);
+                }catch(Exception e){
+                    joinChannel("#"+sender.toLowerCase());
+                    }
 
-    }
-}
+
+                }
+
+            }
+        }
---
+++ src/FallbotMain.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Fallout301 on 6/5/14.
+ */
+public class FallbotMain {
+//This Class is basically the init junk
+    public static void main(String[] args) throws Exception {
+        String Server = "";
+        String Password = "";
+        Integer Port = 6667;
+        Fallbot bot = new Fallbot();
+
+        // Enable debugging output.
+        bot.setVerbose(true);
+
+        // Connect to the IRC server.
+        if(Password!=""){
+        bot.connect(Server,Port,Password);
+        }
+        if(Password == ""){
+            bot.connect(Server,Port);
+        }
+        // Join the #pircbot channel.
+        bot.joinChannel("#fallbot301");
+
+    }
+}
+
