2663deac9071dbc76a0cd2af78f96ccd12c33d2e
fallout301 <fallout.3.for.life@gmail.com>
2014-10-21 21:46:16 -0400

    Some MySQL Integration, Made the code less derpy.

--- src/Fallbot.java
+++ src/Fallbot.java
@@ -1,47 +1,61 @@
+import java.util.Date;
 import org.jibble.pircbot.PircBot;
-import org.mariadb.jdbc.*;
 
-/**
- * Created by Fallout301 on 3/14/14.
- */
-public class Fallbot extends PircBot {
-
+public class Fallbot extends PircBot
+{
+    String Owner = "fallout301";
     String Name = "Fallbot301";
+    public static String Channel = null;
+    public String Message = null;
+    public String Sender = null;
 
-    public Fallbot(){
-        this.setName(Name);
+    Integer UserLevel = null;
+    String Comm = null;
+    String Resp = null;
+
+    public Fallbot()
+    {
+        setName(this.Name);
     }
 
-    @Override
-    protected void onJoin(String channel, String sender, String login, String hostname) {
-
+    protected void onJoin(String channel, String sender, String login, String hostname)
+    {
     }
 
-    public void onMessage(String channel, String sender,
-                          String login, String hostname, String message) {
-        //Eh, why not.
-        if (message.equalsIgnoreCase("time")) {
-            String time = new java.util.Date().toString();
+    public void onMessage(String channel, String sender, String login, String hostname, String message)
+    {
+        Channel = channel;
+        this.Message = message;
+        this.Sender = sender;
+        if (message.equalsIgnoreCase("!table")) {
+            SQLInteraction.createTable();
+            sendMessage(Channel, SQLInteraction.TMessage);
+        }
+        if (message.equalsIgnoreCase("!addcom")) {
+            String Chan = Channel.replace("#", "").toUpperCase();
+
+            SQLInteraction.addCom(Chan, this.Comm, this.Resp, this.UserLevel);
+        }
+
+        if (message.equalsIgnoreCase("!time")) {
+            String time = new Date().toString();
             sendMessage(channel, sender + ": The time is now " + time);
         }
-        if (message.equalsIgnoreCase("!dismiss")& channel.contains(sender)){
-            sendMessage(channel,"Understood, peace out!");
-            try{
-            wait(1,1);
-            }catch(Exception e){
-                partChannel(channel);
-            }
+        if (((message.equalsIgnoreCase("!dismiss") & channel.contains(sender))) || ((message.equalsIgnoreCase("!dismiss") & sender.equalsIgnoreCase(this.Owner)))) {
+            sendMessage(channel, "Understood, peace out!");
+            partChannel(channel);
         }
-        if(message.equalsIgnoreCase("!join")& channel.contains(getNick().toLowerCase())){
+        if ((message.equalsIgnoreCase("!join") & channel.contains(getNick().toLowerCase()))) {
             sendMessage(channel, "I shall arrive shortly!");
-            try{
-                wait(1,1);
-                }catch(Exception e){
-                    joinChannel("#"+sender.toLowerCase());
-                    }
-
-                }
-
-            }
-
+            joinChannel("#" + sender.toLowerCase());
+            sendMessage("#" + sender.toLowerCase(), "Hi I'm Fallbot! Add me as a mod so I can do the modded things (not implemented yet)");
         }
+
+        if ((message.contains("!join #") & channel.contains(getNick().toLowerCase()))) {
+            sendMessage(channel, "Affirmative");
+            String[] parts = message.split(" ");
+            joinChannel(parts[1]);
+            sendMessage(parts[1], "Hi I'm Fallbot! add me as a mod so I can do the  modded things (not implemented yet)");
+        }
+    }
+}
--- src/FallbotMain.java
+++ src/FallbotMain.java
@@ -1,27 +1,22 @@
-/**
- * Created by Fallout301 on 6/5/14.
- */
-public class FallbotMain {
-//This Class is basically the init junk
-    public static void main(String[] args) throws Exception {
+public class FallbotMain
+{
+    public static void main(String[] args)
+            throws Exception
+    {
         String Server = "";
         String Password = "";
         Integer Port = 6667;
         Fallbot bot = new Fallbot();
 
-        // Enable debugging output.
         bot.setVerbose(true);
 
-        // Connect to the IRC server.
-        if(Password!=""){
-        bot.connect(Server,Port,Password);
+        if (Password != "") {
+            bot.connect(Server, Port, Password);
         }
-        if(Password == ""){
-            bot.connect(Server,Port);
+        if (Password == "") {
+            bot.connect(Server, Port);
         }
-        // Join the #fallbot301 channel.
-        bot.joinChannel("#fallbot301");
 
+        bot.joinChannel("#fallbot301");
     }
 }
-
